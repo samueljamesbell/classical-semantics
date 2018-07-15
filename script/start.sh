ca15ef2fbe2b874d9bef9e79fdd6c918e87278f6
Samuel Bell <samueljamesbell@gmail.com>
2018-07-15 20:23:49 +0100

    Update start script to ssh and jump straight into tmux

--- script/start.sh
+++ script/start.sh
@@ -2,5 +2,7 @@
 
 gcloud compute instances start birch-vm
 
-gcloud compute ssh sam@birch-vm -- -L 8889:localhost:8889
+gcloud compute ssh sam@birch-vm \
+    --command "cd ~/projects/classical-semantics ; ./script/start-jupyter.sh" \
+    -- -t -L 8889:localhost:8889
 
