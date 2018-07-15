4008704dc468c18771b2bdbf1e479d171b59b4e8
Samuel Bell <samueljamesbell@gmail.com>
2018-07-15 19:22:13 +0000

    Update start-jupyter.sh to start more useful tmux session

--- script/start-jupyter.sh
+++ script/start-jupyter.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-tmux new-session "jupyter lab --no-browser --port=8889"
+tmux new-session -s "classical-semantics" -d
+tmux send-keys -t "classical-semantics" "jupyter lab --no-browser --port=8889" C-m
+tmux split-window -v -t "classical-semantics" 
+tmux send-keys -t "classical-semantics" "htop" C-m
+tmux split-window -h -t "classical-semantics" 
+tmux send-keys -t "classical-semantics" "watch nvidia-smi" C-m
+tmux attach -t "classical-semantics"
