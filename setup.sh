8cd71d1faff94bef165f259db999848010243383
Samuel Bell <samueljamesbell@gmail.com>
2018-07-28 16:24:17 +0100

    Stop using pipenv; replace with pyenv and virtualenv
    Former-commit-id: 6bc137e8ab8d8b32c219663fc993a613059673e0

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-brew bundle
+pyenv install 3.6.6
 
-pipenv install --ignore-pipfile --dev
+cd ..
+pyenv virtualenv 3.6.6 classical-semantics
+cd classical-semantics
+
+pip install -r requirements.txt
 
 echo "Done"
