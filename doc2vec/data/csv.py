2f126a1bf71327b4704c1da287322dcd196ab125
Samuel Bell <samueljamesbell@gmail.com>
2018-08-12 21:38:21 +0100

    Remove doc2vec
    Former-commit-id: 3988165b1c9571f757db95c6bab4f4b7ab00769d

--- doc2vec/data/csv.py
+++
@@ -1,6 +0,0 @@
-def composers(path):
-    """Yield composer tuples from CSV."""
-    with open(path, 'r') as f:
-        next(f)  # Skip headers
-        for composer_id, line in enumerate(f):
-            yield (composer_id, *line.strip().split('|'))
